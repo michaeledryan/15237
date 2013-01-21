4d02e4bc9b855c8c92e8f52aa3913721434ab98b
Michael Ryan <michaeledryan@gmail.com>
2013-01-21 17:10:09 -0500

    ahhhhhh

--- main.js
+++ main.js
@@ -1,25 +1,70 @@
-function init() {
+var SCREEN_WIDTH = 800,
+    SCREEN_HEIGHT = 600;        
+   
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+      
+var xpos = 142, 
+  ypos = 36, 
+  index = 0, 
+  numFrames = 30, 
+  frameX = 20,
+  frameY = 25;
+   
+ //load the image
+image = new Image();
+image.src = "sprites/megaman.gif";
 
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.font = "60px Arial";
-  ctx.textAlign = "center";
-  ctx.fillText("Hello World!", 200, 200);
+image.onload = function() {
 
-  ctx.font = "60px Arial";
-  ctx.textAlign = "center";
-  ctx.fillStyle = "blue";
-  ctx.fillText("Hello World!", 200, 200);
+    //we're ready for the loop
+    init();
+}
+       
 
-  var text = "Go Steelers!";
-  ctx.font = "bold italic 30px sans-serif";
-  ctx.textAlign = "right";
-  ctx.textBaseline = "middle";
-  ctx.strokeStyle = "green";
-  ctx.strokeText(text, 400, 100);
+function init(){
+  //we're ready for the loop
 
-  var width = ctx.measureText(text).width;
-  ctx.strokeStyle = "orange";
-  ctx.strokeRect(400-width,100-30/2,width,30);
+  beginStage();
 
+
+  setInterval(loop, 1000);
+}
+
+
+function beginStage() {};
+
+function drawMegaMan() {};
+
+function loop() {
+  //clear the canvas!
+  ctx.clearRect(0,0, SCREEN_HEIGHT,SCREEN_WIDTH);
+  
+  drawMegaMan();
+
+  /*our big long list of arguments below equates to: 
+      1: our image source
+      2 - 5: the rectangle in the source image of what we want to draw
+      6 - 9: the  rectangle of our canvas that we are drawing into
+        
+      the area of the source image we are drawing from will change each time loop() is called.
+      the rectangle of our canvas that we are drawing into however, will not. 
+      tricky!
+  */
+  ctx.drawImage(image,xpos,ypos,frameX,frameY,0,0,frameX, frameY);
+  
+  //each time around we add the frame size to our xpos, moving along the source image
+  //increase the index so we know which frame of our animation we are currently on
+  index += 1;
+  
+  //if our index is higher than our total number of frames, we're at the end and better start over
+  if (index >= 2) {
+      xpos -= 31;
+      index=0;    
+  //if we've gotten to the limit of our source image's width, we need to move down one row of frames                
+  } else {
+      xpos += 31;
+  }
+  
+  
 }
